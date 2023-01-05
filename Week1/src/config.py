Found problem. Was applying softmax before using crossentropyloss (which does a softmax). Fixed now.

Week1/src/config.py
@@ -3,24 +3,22 @@ import torch as T
 
 train_configs = {}
 
-train_configs['bert_base_no_bias_no_dropout'] = {
-        'n_epochs': 1,
+train_configs['bert_base'] = {
+        'n_epochs': 4,
         'lr': 5e-4,
-        'batch_size': 1024,
+        'batch_size': 1536,
         'dtype': T.float16,
         'model_file': '../trained_models/CrammingModel.pt',
-        'num_workers': 6,
+        'num_workers': 4,
         'pin_memory': True,
-        'input_dims': 128,
+        'seq_length': 128,
         'embed_dims': 768, 
-        'num_heads': 12, 
-        'has_bias': False,
-        'dropout_rate': 0.0,
-        'mlp_hidden_dims': None,
+        'num_heads': 12,
+        'has_bias': True,
+        'dropout_rate': 0.1,
         'n_encoder_blocks': 12,
         'mlp_expansion_factor': 4,
-        'output_dims': None,
         'use_gpu': True,
-        'loss_fn': T.nn.CrossEntropyLoss()
+        'loss_fn': T.nn.CrossEntropyLoss(ignore_index=-100)
         }
 

Week1/src/handler.py
@@ -44,7 +44,7 @@ class DataHandler:
         ## Concatenate dataset to be of max_length.
         self.condense_dataset()
 
-        self.tokenizer   = BertTokenizer.from_pretrained('bert-base-cased')
+        self.tokenizer   = BertTokenizer.from_pretrained('bert-base-cased', cls_token='')
         self.vocab_size  = len(self.tokenizer)
         self.device      = device
         self.dtype       = dtype
@@ -112,6 +112,7 @@ class CustomCollator:
 
         self.pad_token_id   = self.tokenizer(['[PAD]'], add_special_tokens=False)['input_ids'][0][0]
         self.mask_token_id  = self.tokenizer(['[MASK]'], add_special_tokens=False)['input_ids'][0][0]
+        self.sep_token_id   = self.tokenizer(['[SEP]'], add_special_tokens=False)['input_ids'][0][0]
 
 
     def __call__(self, batch) -> (T.tensor, T.tensor):
@@ -142,13 +143,16 @@ class CustomCollator:
         original_ids    = token_ids.clone().flatten()
         token_idxs      = T.arange(0, original_ids.shape[0])
 
+        '''
         original_labels = T.zeros((token_ids.shape[0] * token_ids.shape[1], self.tokenizer.vocab_size))
         original_labels[token_idxs, original_ids] = 1
+        '''
 
         ## mask (i.e. (1, 0, 0, 1, ...)) where mask tokens (i.e. '[MASK]') are applied.
         mask_mask = T.zeros_like(attention_mask)
-        for idx, batch in enumerate(attention_mask):
-            nonpad_idxs = T.argwhere(batch).squeeze()
+        ##for idx, batch in enumerate(attention_mask):
+        for idx, batch in enumerate(token_ids):
+            nonpad_idxs = T.argwhere(batch != self.sep_token_id).squeeze()
             mask_idxs = np.random.choice(
                     nonpad_idxs, 
                     size=int(nonpad_idxs.shape[0] * self.mlm_prob), 
@@ -159,8 +163,8 @@ class CustomCollator:
 
         mask_idxs = T.argwhere(mask_mask.flatten() != 0).squeeze()
 
-        original_labels = original_labels[mask_idxs].type(T.float16)
-        return token_ids, attention_mask, original_labels, mask_idxs 
+        #original_ids[mask_idxs] = -100
+        return token_ids, attention_mask, original_ids[mask_idxs], mask_idxs 
 
 
 

Week1/src/main.py
@@ -13,26 +13,25 @@ from config import train_configs
 
 
 
-DEBUG            = True 
+SHOW_PROGRESS    = True
+DEBUG            = False 
 MICRO_BATCH_SIZE = 32
 
 def train(
         n_epochs=1,
         lr=1e-4,
-        batch_size=1024,
+        batch_size=1536,
         dtype=T.float16,
         model_file='../trained_models/CrammingModel.pt',
         num_workers=4,
         pin_memory=True,
-        input_dims=128,
+        seq_length=128,
         embed_dims=768, 
         num_heads=12, 
-        has_bias=False,
-        dropout_rate=0.0,
-        mlp_hidden_dims=None,
+        has_bias=True,
+        dropout_rate=0.2,
         n_encoder_blocks=12,
         mlp_expansion_factor=4,
-        output_dims=None,
         use_gpu=True,
         loss_fn=T.nn.CrossEntropyLoss()
         ) -> None:
@@ -42,7 +41,7 @@ def train(
     device  = T.device('cuda:0' if use_gpu else 'cpu')
     handler = DataHandler(
             dataset_name='bookcorpus', 
-            max_length=input_dims,
+            max_length=seq_length,
             dtype=dtype,
             device=device
             )
@@ -53,66 +52,77 @@ def train(
 
     cramming_model = CrammingTransformer(
             vocab_size=handler.tokenizer.vocab_size,
-            input_dims=input_dims,
+            seq_length=seq_length,
             embed_dims=embed_dims,
             num_heads=num_heads, 
             has_bias=has_bias,
             dropout_rate=dropout_rate,
-            mlp_hidden_dims=mlp_hidden_dims,
             n_encoder_blocks=n_encoder_blocks,
             mlp_expansion_factor=mlp_expansion_factor,
             lr=lr,
             device=device
             )
 
-    grad_scaler = GradScaler()
-
     progress_bar = tqdm(total=len(dataloader) * n_epochs)
 
     back_losses = []
-    for idx, (X, attention_mask, y, mask_idxs) in enumerate(dataloader):
-        X              = X.to(cramming_model.device)
-        attention_mask = attention_mask.to(cramming_model.device)
-        y              = y.to(cramming_model.device)
-        mask_idxs      = mask_idxs.to(cramming_model.device)
+    for epoch in range(n_epochs):
+        for idx, (X, attention_mask, y, mask_idxs) in enumerate(dataloader):
+            X              = X.to(cramming_model.device)
+            attention_mask = attention_mask.to(cramming_model.device)
+            y              = y.to(cramming_model.device)
+            mask_idxs      = mask_idxs.to(cramming_model.device)
 
-        with T.autocast(device_type='cuda', dtype=T.float16):
-            out = cramming_model.forward(X, attention_mask)
-            out = out.flatten(start_dim=0, end_dim=-2)
-            out = out[mask_idxs]
-            
-            loss = loss_fn(out, y)
+            with T.cuda.amp.autocast():
+                out = cramming_model.forward(X, attention_mask)
+                out = out.flatten(start_dim=0, end_dim=-2)
+                out = out[mask_idxs]
+                
+                loss = loss_fn(out, y)
 
-        T.nn.utils.clip_grad_norm_(cramming_model.parameters(), max_norm=2.0)
-        grad_scaler.scale(loss).backward()
 
-        if idx % (batch_size // MICRO_BATCH_SIZE) == 0:
-            grad_scaler.step(cramming_model.optimizer)
-            grad_scaler.update()
-            cramming_model.scheduler.step()
+            loss.backward()
 
-            ## Zeroing grad like this is faster. (https://h-huang.github.io/tutorials/recipes/recipes/tuning_guide.html)
-            cramming_model.optimizer.zero_grad()
-            for param in cramming_model.parameters():
-                param.grad = None
-            
-        back_losses.append(loss.item())
+            if idx % (batch_size // MICRO_BATCH_SIZE) == 0:
+                T.nn.utils.clip_grad_value_(cramming_model.parameters(), clip_value=0.5)
+                cramming_model.optimizer.step()
+                ##cramming_model.scheduler.step()
 
-        if idx % 1000 == 0:
-            cramming_model.save_model(model_file=model_file)
+                cramming_model.optimizer.zero_grad()
+                '''
+                ## Zeroing grad like this is faster. (https://h-huang.github.io/tutorials/recipes/recipes/tuning_guide.html)
+                for param in cramming_model.parameters():
+                    param.grad = None
+                '''
+                
+            back_losses.append(loss.item())
 
-            ## Prediction sample
-            if DEBUG:
-                print(
-                        f'Original text True:    {handler.tokenizer.decode(X[0])}\n\n', 
-                        f'Predicted Mask Tokens: {handler.tokenizer.decode(T.argmax(out[:128], dim=-1))}\n\n'
-                        f'Actual Mask text:      {handler.tokenizer.decode(T.argmax(y[:128], dim=-1))}\n\n', 
-                        )
+            if len(back_losses) == 500:
+                back_losses.pop(0)
 
-        progress_bar.update(1)
-        progress_bar.set_description(f'Running Loss: {np.mean(back_losses[-100:])}')
+            if idx % 1000 == 0:
+                print(f'Tokens ingested: {idx * 128 * MICRO_BATCH_SIZE // 1e6}M')
+                cramming_model.save_model(model_file=model_file)
+
+                ## Prediction sample
+                if SHOW_PROGRESS:
+                    print(
+                            f'Original text:         {handler.tokenizer.decode(X.flatten()[:128])}\n\n', 
+                            f'Predicted Mask Tokens: {handler.tokenizer.decode(T.argmax(out[:128], dim=-1))}\n\n'
+                            f'Actual Mask text:      {handler.tokenizer.decode(y[:128])}\n\n',
+                            )
+                if DEBUG:
+                    print(
+                            f'Should be all mask:    {handler.tokenizer.decode(X.flatten()[mask_idxs])}\n\n'
+                            f'Actual Mask text:      {handler.tokenizer.decode(T.argmax(y[:128], dim=-1))}\n\n', 
+                            f'Output Tensor:         {out}\n\n'
+                            )
+
+
+            progress_bar.update(1)
+            progress_bar.set_description(f'Running Loss: {np.mean(back_losses[-100:])}')
 
 
 
 if __name__ == '__main__':
-    train(**train_configs['bert_base_no_bias_no_dropout'])
+    train(**train_configs['bert_base'])

Week1/src/model.py
@@ -20,7 +20,7 @@ class InputEmbedding(nn.Module):
             embed_dims=768, 
             max_seq_length=512, 
             dtype=T.float16, 
-            device=T.device('cpu')
+            device=T.device('cuda:0' if T.cuda.is_available() else 'cpu')
             ) -> None:
         super(InputEmbedding, self).__init__()
 
@@ -45,7 +45,7 @@ class InputEmbedding(nn.Module):
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, embed_dims=768, num_heads=8, has_bias=False) -> None:
+    def __init__(self, embed_dims=768, num_heads=8, has_bias=False, dropout_rate=0.0) -> None:
         super(SelfAttention, self).__init__()
         
         self.embed_dims = embed_dims
@@ -55,20 +55,22 @@ class SelfAttention(nn.Module):
         assert(self.head_dims * num_heads == embed_dims), "Embed dims needs to be divisible by heads"
 
         ## Concat qkv into one matrix
-        self.qkv    = nn.Linear(embed_dims, 3 * embed_dims, bias=False)
-        self.fc_out = nn.Linear(embed_dims, embed_dims, bias=has_bias)
+        self.qkv     = nn.Linear(embed_dims, 3 * embed_dims, bias=False)
+        self.dropout = nn.Dropout1d(p=dropout_rate)
+        self.fc_out  = nn.Linear(embed_dims, embed_dims, bias=has_bias)
 
 
     @staticmethod
     def scaled_dot_product(query, key, value, attention_mask=None) -> T.tensor:
-        embed_dim = key.shape[-1]
-        attention_logits = T.matmul(query, key.transpose(-2, -1)) / math.sqrt(embed_dim)
+        embed_dims = key.shape[-1]
+        attention_logits = T.matmul(query, key.transpose(-2, -1)) / math.sqrt(embed_dims)
 
         if attention_mask is not None:
             mask_broadcast = attention_mask.unsqueeze(dim=1).unsqueeze(dim=1)
 
             ## Fill with fp16 min value
             attention_logits = attention_logits.masked_fill(mask_broadcast == 0, -1e+4)
+
         attention = F.softmax(attention_logits, dim=-1)
         values = T.matmul(attention, value)
         return values
@@ -92,7 +94,8 @@ class SelfAttention(nn.Module):
         out = out.permute(0, 2, 1, 3)
         out = out.reshape(batch_size, seq_length, self.embed_dims)
 
-        X = self.fc_out(out)
+        X = self.dropout(out)
+        X = self.fc_out(X)
         return X
 
 
@@ -103,19 +106,18 @@ class Encoder(nn.Module):
             num_heads=8,
             has_bias=False,
             dropout_rate=0.0,
-            mlp_hidden_dims=None,
             mlp_expansion_factor=4
             ) -> None:
         super(Encoder, self).__init__()
 
-        if mlp_hidden_dims is None:
-            mlp_hidden_dims = mlp_expansion_factor * embed_dims
+        mlp_hidden_dims = mlp_expansion_factor * embed_dims
 
         self.attention_norm = nn.LayerNorm(embed_dims)
         self.attention_block = SelfAttention(
                 embed_dims=embed_dims,
                 num_heads=num_heads,
-                has_bias=has_bias
+                has_bias=has_bias,
+                dropout_rate=dropout_rate
                 )
 
         self.mlp_norm = nn.LayerNorm(embed_dims)
@@ -127,23 +129,25 @@ class Encoder(nn.Module):
         
 
     def forward(self, X: T.tensor, attention_mask: T.tensor = None) -> (T.tensor, T.tensor):
-        '''
         ## Post-norm
-        attention_output = self.attention_block(X, attention_mask)
-        X = self.attention_norm(attention_output + X)
-        mlp_output = self.mlp_block(X)
-        X = self.mlp_norm(mlp_output)
+        _X = self.attention_block(X, attention_mask)
+        X  = self.attention_norm(X + _X)
+        
+        _X = fused_gelu(self.dropout1(self.fc1(X)))
+        _X = self.dropout2(self.fc2(_X))
+        X  = self.mlp_norm(X + _X)
+
         '''
-
         ## Pre-norm
-        X = self.attention_norm(X)
-        attention_output = self.attention_block(X, attention_mask)
-        X = self.mlp_norm(attention_output)
-
-        X = fused_gelu(self.fc1(X))
-        X = self.dropout1(X)
-        X = fused_gelu(self.fc2(X))
-        X = self.dropout2(X)
+        _X = self.attention_norm(X)
+        X  = X + self.attention_block(_X, attention_mask)
+        
+        _X = self.mlp_norm(X)
+        _X = fused_gelu(self.fc1(_X))
+        _X = self.dropout1(_X)
+        _X = fused_gelu(self.fc2(_X))
+        X  = X + self.dropout2(_X)
+        '''
         return X, attention_mask
 
 
@@ -151,12 +155,11 @@ class CrammingTransformer(nn.Module):
     def __init__(
             self,
             vocab_size,
-            input_dims,
+            seq_length,
             embed_dims=384, 
             num_heads=8, 
             has_bias=False,
             dropout_rate=0.0,
-            mlp_hidden_dims=None,
             n_encoder_blocks=8,
             mlp_expansion_factor=4,
             lr=5e-4,
@@ -168,7 +171,7 @@ class CrammingTransformer(nn.Module):
         self.input_embedding = InputEmbedding(
                 vocab_size=vocab_size,
                 embed_dims=embed_dims,
-                max_seq_length=input_dims,
+                max_seq_length=seq_length,
                 device=device
                 )
         self.model = nn.ModuleList([
@@ -177,22 +180,29 @@ class CrammingTransformer(nn.Module):
                 num_heads=num_heads,
                 has_bias=has_bias,
                 dropout_rate=dropout_rate,
-                mlp_hidden_dims=mlp_hidden_dims,
                 mlp_expansion_factor=mlp_expansion_factor
                 ) for _ in range(n_encoder_blocks)
             ])
 
         self.classifier_head = nn.Sequential(
                 nn.LayerNorm(embed_dims),
-                nn.Linear(embed_dims, vocab_size, bias=has_bias),
-                nn.Softmax(dim=-1)
+                nn.Linear(embed_dims, vocab_size, bias=has_bias)
+                #nn.Softmax(dim=-1)
             )
 
-        self.optimizer = optim.Adam(self.parameters(), lr=lr, weight_decay=0.01)
+        self.optimizer = optim.Adam(
+                self.parameters(), 
+                lr=lr, 
+                weight_decay=0.01, 
+                eps=1e-12,
+                betas=(0.9, 0.98)
+                )
+        '''
         self.scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(
                 self.optimizer, 
                 T_0=10000
                 )
+        '''
 
         self.device = device
         self.to(self.device)
@@ -201,7 +211,7 @@ class CrammingTransformer(nn.Module):
     def forward(self, X: T.tensor, attention_mask: T.tensor = None) -> T.tensor:
         X = self.input_embedding(X)
         for encoder_block in self.model:
-            X, _ = encoder_block(X, attention_mask)
+            X, attention_mask = encoder_block(X, attention_mask)
         return self.classifier_head(X)
 
 